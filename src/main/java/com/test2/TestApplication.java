caf7d6c574aa550c8477910a3c99128ce2c52004
vaman <vamany4@gmail.com>
2025-03-13 12:36:28 +0530

    Defect-3456-login -error-closed

--- src/main/java/com/test2/TestApplication.java
+++ src/main/java/com/test2/TestApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
-
+          
 	public static void main(String[] args) {
+                int x =100;
 		SpringApplication.run(TestApplication.class, args);
 	}
 
